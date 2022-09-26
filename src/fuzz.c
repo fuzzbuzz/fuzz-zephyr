20d80502e05b2354ff72090f2bfbf89bf266b029
Alexander Guy <alexander@fuzzbuzz.io>
2022-09-26 16:04:38 -0700

    Remove everything but the LLVMFuzzerTestOneInput entry point

--- src/fuzz.c
+++ src/fuzz.c
@@ -34,82 +34,6 @@
 #include "cmdline.h"
 #include "irq_ctrl.h"
 
-void posix_exit(int exit_code)
-{
-	static int max_exit_code;
-
-	max_exit_code = MAX(exit_code, max_exit_code);
-	/*
-	 * posix_soc_clean_up may not return if this is called from a SW thread,
-	 * but instead it would get posix_exit() recalled again
-	 * ASAP from the HW thread
-	 */
-	posix_soc_clean_up();
-	hwm_cleanup();
-	native_cleanup_cmd_line();
-	exit(max_exit_code);
-}
-
-/**
- * Run all early native_posix initialization steps, including command
- * line parsing and CPU start, until we are ready to let the HW models
- * run via hwm_one_event()
- */
-void posix_init(int argc, char *argv[])
-{
-	run_native_tasks(_NATIVE_PRE_BOOT_1_LEVEL);
-
-	native_handle_cmd_line(argc, argv);
-
-	run_native_tasks(_NATIVE_PRE_BOOT_2_LEVEL);
-
-	hwm_init();
-
-	run_native_tasks(_NATIVE_PRE_BOOT_3_LEVEL);
-
-	posix_boot_cpu();
-
-	run_native_tasks(_NATIVE_FIRST_SLEEP_LEVEL);
-}
-
-/**
- * Execute the simulator for at least the specified timeout, then
- * return.  Note that this does not affect event timing, so the "next
- * event" may be significantly after the request if the hardware has
- * not been configured to e.g. send an interrupt when expected.
- */
-void posix_exec_for(uint64_t us)
-{
-	uint64_t start = hwm_get_time();
-
-	do {
-		hwm_one_event();
-	} while (hwm_get_time() < (start + us));
-}
-
-#ifndef CONFIG_ARCH_POSIX_LIBFUZZER
-
-/**
- * This is the actual host process main routine.  The Zephyr
- * application's main() is renamed via preprocessor trickery to avoid
- * collisions.
- *
- * Not used when building fuzz cases, as libfuzzer has its own main()
- * and calls the "OS" through a per-case fuzz test entry point.
- */
-int main(int argc, char *argv[])
-{
-	posix_init(argc, argv);
-	while (true) {
-		hwm_one_event();
-	}
-
-	/* This line should be unreachable */
-	return 1; /* LCOV_EXCL_LINE */
-}
-
-#else /* CONFIG_ARCH_POSIX_LIBFUZZER */
-
 /**
  * Entry point for fuzzing (when enabled). Works by placing the data
  * into two known symbols, triggering an app-visible interrupt, and
@@ -142,5 +66,3 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t sz)
 
 	return 0;
 }
-
-#endif
